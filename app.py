8f592b208c2df272e9e15d7f929765a2c56e20d7
Kaartik Nayak <kaartiknayak2001@gmail.com>
2021-04-27 17:32:19 +0530

    python file updated

---
+++ app.py
@@ -0,0 +1,8 @@
+import requests
+import pafy
+link = input("Enter Link of The YT Video: ")
+try:
+    data = pafy.new(link).streams[0].download()
+    print("Your Video is downloaded 🎉")
+except ValueError:
+    print("PLease Enter an appropriate Link 🙂")
